37d0ff7b463d0096d6da9227c735da108fb57777
Robert Anton Reese <robert@rrreese.com>
2012-11-03 08:13:54 +0000

    Added test for method to determine fit size

---
+++ Utilities/ImageHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class ImageHelper
+    {
+        /// <summary>
+        /// Based on the dimensions of the old image rectangle, returns the dimensions for a new image rectangle that fits within the new dimensions. 
+        /// This will result in a letterboxed rectangle that has the same aspect ratio as the original rectangle.
+        /// </summary>
+        public static Tuple<int,int> ResizeDimensions(int imageWidth, int imageHeight, int fitWidth, int fitHeight)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UtilitiesTests/ImageHelperTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utilities;
+
+namespace UtilitiesTests
+{
+    [TestClass]
+    public class ImageHelperTest
+    {
+        [TestMethod]
+        public void BothImageDimensionsLargerThenFitDiemnsions()
+        {
+            var point = ImageHelper.ResizeDimensions(1024, 720, 600, 600);
+            Assert.AreEqual(point.Item1, 600);
+            Assert.AreEqual(point.Item2, 422);
+        }
+
+        [TestMethod]
+        public void XImageDimensionsLargerThenFitDiemnsions()
+        {
+            var point = ImageHelper.ResizeDimensions(1024, 720, 800, 800);
+            Assert.AreEqual(point.Item1, 800);
+            Assert.AreEqual(point.Item2, 563);
+        }
+
+        [TestMethod]
+        public void YImageDimensionsLargerThenFitDiemnsions()
+        {
+            var point = ImageHelper.ResizeDimensions(720, 1024, 800, 800);
+            Assert.AreEqual(point.Item1, 563);
+            Assert.AreEqual(point.Item2, 800);
+        }
+
+        [TestMethod]
+        public void BothImageDimensionsSmallerThenFitDiemnsions()
+        {
+            var point = ImageHelper.ResizeDimensions(1024, 720, 1200, 1200);
+            Assert.AreEqual(point.Item1, 1024);
+            Assert.AreEqual(point.Item2, 720);
+        }
+
+        [TestMethod]
+        public void ImageDimensionsEqualFitDiemnsions()
+        {
+            var point = ImageHelper.ResizeDimensions(1024, 720, 1024, 720);
+            Assert.AreEqual(point.Item1, 1024);
+            Assert.AreEqual(point.Item2, 720);
+        }
+    }
+}
