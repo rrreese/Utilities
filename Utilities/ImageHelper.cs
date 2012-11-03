b449a86b8f5f73b7e9e2e3ee3d3d199eeb43d8af
Robert Anton Reese <robert@rrreese.com>
2012-11-03 09:56:59 +0000

    Changed from tuple to point class

--- Utilities/ImageHelper.cs
+++ Utilities/ImageHelper.cs
@@ -13,24 +13,40 @@ namespace Utilities
         /// Based on the dimensions of the old image rectangle, returns the dimensions for a new image rectangle that fits within the new dimensions. 
         /// This will result in a letterboxed rectangle that has the same aspect ratio as the original rectangle.
         /// </summary>
-        public static Tuple<int,int> ResizeDimensions(double imageWidth, double imageHeight, double fitWidth, double fitHeight)
+        public static Point ResizeDimensions(double imageWidth, double imageHeight, double fitWidth, double fitHeight)
         {
             if (imageWidth <= fitWidth && imageHeight <= fitHeight)
             {
-                return new Tuple<int, int>((int)imageWidth, (int)imageHeight);
+                return new Point((int)imageWidth, (int)imageHeight);
             }
 
             if (imageWidth > fitWidth)
             {
-                return new Tuple<int, int>((int) fitWidth, (int) Math.Round((fitWidth/imageWidth)*imageHeight));
+                return new Point((int) fitWidth, (int) Math.Round((fitWidth/imageWidth)*imageHeight));
             }
 
             if (imageHeight > fitHeight)
             {
-                return new Tuple<int, int>((int)Math.Round((fitHeight / imageHeight) * imageWidth), (int)fitHeight);
+                return new Point((int)Math.Round((fitHeight / imageHeight) * imageWidth), (int)fitHeight);
             }
-         
-            return new Tuple<int, int>(0, 0);
+
+            return new Point(0, 0);
+        }
+
+
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
         }
     }
+
+
+
 }
--- UtilitiesTests/ImageHelperTest.cs
+++ UtilitiesTests/ImageHelperTest.cs
@@ -11,61 +11,61 @@ namespace UtilitiesTests
         public void BothImageDimensionsLargerThenFitDiemnsions()
         {
             var point = ImageHelper.ResizeDimensions(1024, 720, 600, 600);
-            Assert.AreEqual(point.Item1, 600);
-            Assert.AreEqual(point.Item2, 422);
+            Assert.AreEqual(point.X, 600);
+            Assert.AreEqual(point.Y, 422);
         }
 
         [TestMethod]
         public void XImageDimensionsLargerThenFitDiemnsions()
         {
             var point = ImageHelper.ResizeDimensions(1024, 720, 800, 800);
-            Assert.AreEqual(point.Item1, 800);
-            Assert.AreEqual(point.Item2, 562);
+            Assert.AreEqual(point.X, 800);
+            Assert.AreEqual(point.Y, 562);
         }
 
         [TestMethod]
         public void YImageDimensionsLargerThenFitDiemnsions()
         {
             var point = ImageHelper.ResizeDimensions(720, 1024, 800, 800);
-            Assert.AreEqual(point.Item1, 562);
-            Assert.AreEqual(point.Item2, 800);
+            Assert.AreEqual(point.X, 562);
+            Assert.AreEqual(point.Y, 800);
         }
 
         [TestMethod]
         public void BothImageDimensionsSmallerThenFitDiemnsions()
         {
             var point = ImageHelper.ResizeDimensions(1024, 720, 1200, 1200);
-            Assert.AreEqual(point.Item1, 1024);
-            Assert.AreEqual(point.Item2, 720);
+            Assert.AreEqual(point.X, 1024);
+            Assert.AreEqual(point.Y, 720);
         }
 
         [TestMethod]
         public void ImageDimensionsEqualFitDiemnsions()
         {
             var point = ImageHelper.ResizeDimensions(1024, 720, 1024, 720);
-            Assert.AreEqual(point.Item1, 1024);
-            Assert.AreEqual(point.Item2, 720);
+            Assert.AreEqual(point.X, 1024);
+            Assert.AreEqual(point.Y, 720);
         }
 
         [TestMethod]
         public void ImageDimensionsWiderButHeightOutsideFitWidthInsideFit()
         {
             var point = ImageHelper.ResizeDimensions(790, 610, 800, 600);
-            Assert.AreEqual(point.Item1, 777);
-            Assert.AreEqual(point.Item2, 600);
+            Assert.AreEqual(point.X, 777);
+            Assert.AreEqual(point.Y, 600);
         }
 
         [TestMethod]
         public void ZeroCase()
         {
             var point = ImageHelper.ResizeDimensions(0, 0, 800, 600);
-            Assert.AreEqual(point.Item1, 0);
-            Assert.AreEqual(point.Item2, 0);
+            Assert.AreEqual(point.X, 0);
+            Assert.AreEqual(point.Y, 0);
 
 
             point = ImageHelper.ResizeDimensions(800, 800, 0, 0);
-            Assert.AreEqual(point.Item1, 0);
-            Assert.AreEqual(point.Item2, 0);
+            Assert.AreEqual(point.X, 0);
+            Assert.AreEqual(point.Y, 0);
         }
     }
 }
